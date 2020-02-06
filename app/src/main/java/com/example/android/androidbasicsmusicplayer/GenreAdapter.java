c3909089505093122407b21a4048fae84bc0deb9
Thomas Kolonko <tz.kolonko@gmail.com>
2020-02-06 17:50:12 +0100

    Added smaller background images

--- app/src/main/java/com/example/android/androidbasicsmusicplayer/GenreAdapter.java
+++ app/src/main/java/com/example/android/androidbasicsmusicplayer/GenreAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.androidbasicsmusicplayer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
