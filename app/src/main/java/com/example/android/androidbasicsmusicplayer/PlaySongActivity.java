dd738fa2c3ff75b196f4d507ab85a096c2615103
Thomas Kolonko <tz.kolonko@gmail.com>
2020-02-09 09:04:13 +0100

    Adding Toast message to the Buttons in PlaySongActivity

--- app/src/main/java/com/example/android/androidbasicsmusicplayer/PlaySongActivity.java
+++ app/src/main/java/com/example/android/androidbasicsmusicplayer/PlaySongActivity.java
@@ -3,7 +3,12 @@ package com.example.android.androidbasicsmusicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlaySongActivity extends AppCompatActivity {
 
@@ -19,5 +24,37 @@ public class PlaySongActivity extends AppCompatActivity {
             TextView songAuthorPlayView = findViewById(R.id.songAuthorPlayView);
             songAuthorPlayView.setText("by: " + extras.getString("Author"));
         }
+
+        // adding Toast for previous button
+        ImageButton previousButton = findViewById(R.id.playPreviousButton);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Jumping to previous song",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // adding Toast for play / pause button
+        ImageButton playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Playing / Pausing current song",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // adding Toast for next button
+        ImageButton nextButton = findViewById(R.id.playNextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Jumping to the next song",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
